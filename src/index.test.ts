6da5269ac2c13d6a876ad8429ee533cb6d7b778b
nicholas-codecov <nicholas.deschenes@sentry.io>
2023-04-25 08:23:35 -0300

    switch up covered statement

--- src/index.test.ts
+++ src/index.test.ts
@@ -2,10 +2,5 @@ import Index from "./index";
 
 test('test uncovered if', () => {
     const indexObj = new Index();
-    expect(indexObj.uncovered_if()).toEqual(false);
-});
-
-test('fully covered', () => {
-    const indexObj = new Index();
-    expect(indexObj.fully_covered()).toEqual(true);
+    expect(indexObj.uncovered_if(false)).toEqual(false);
 });
